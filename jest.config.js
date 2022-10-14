908a9b50a7d5ab0b0f6107a62d4e5dd5b3178fdb
felixstenian <felixstenian@gmail.com>
2022-10-14 16:05:43 -0300

    update parameter

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  serupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
