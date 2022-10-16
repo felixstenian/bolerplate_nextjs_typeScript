1b25b664a51f9f097d73257867235be5b8c3b996
felixstenian <felixstenian@gmail.com>
2022-10-15 21:36:47 -0300

    test lint-staged pre commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
-    const teste2 = 'nada'
+    const teste3 = 'nada'
 
     try {
       ctx.renderPage = () =>
