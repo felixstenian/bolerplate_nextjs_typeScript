316c14627886e98665405247fbe7f2777db1228d
felixstenian <felixstenian@gmail.com>
2022-10-20 22:38:12 -0300

    creating styles in first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,21 @@
+import * as Style from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h2>React Avançado</h2>
-    </main>
+    <Style.Wrapper>
+      <Style.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e react avançado escrito ao lado."
+      />
+      <Style.Title>React Avançado</Style.Title>
+      <Style.Description>
+        TypeScript, ReactJS, NextJS e Styled Components
+      </Style.Description>
+      <Style.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Desenvolvedor de frete para uam tela com código"
+      />
+    </Style.Wrapper>
   )
 }
 
