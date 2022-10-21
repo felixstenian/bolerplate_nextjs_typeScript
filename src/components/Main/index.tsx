4b8f86c013866cb35d7d7afb97c846c84281847d
felixstenian <felixstenian@gmail.com>
2022-10-21 18:36:55 -0300

    Config @storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,17 @@
 import * as Style from './styles'
 
-const Main = () => {
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => {
   return (
     <Style.Wrapper>
       <Style.Logo
         src="/img/logo.svg"
         alt="Imagem de um átomo e react avançado escrito ao lado."
       />
-      <Style.Title>React Avançado</Style.Title>
-      <Style.Description>
-        TypeScript, ReactJS, NextJS e Styled Components
-      </Style.Description>
+      <Style.Title>{title}</Style.Title>
+      <Style.Description>{description}</Style.Description>
       <Style.Illustration
         src="/img/hero-illustration.svg"
         alt="Desenvolvedor de frete para uam tela com código"
