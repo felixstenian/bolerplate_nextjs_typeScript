0ae7d70f1aacc3af1adf3c7572bb40c7d6af7d69
felixstenian <felixstenian@gmail.com>
2022-10-24 18:27:17 -0300

    update config and exemplae storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,13 +1,20 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Meta, Story } from '@storybook/react'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'React Avançado',
+    description: 'TypeScript, ReactJS, NextJS, storybook e Strapi'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main title={text('Title', 'React Avançado StoryBook')} />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Basic Title: React Avançado',
+  description:
+    'Basic description: TypeScript, ReactJS, NextJS, storybook e Strapi'
+}
+
+export const Default: Story = (args) => <Main {...args} />
